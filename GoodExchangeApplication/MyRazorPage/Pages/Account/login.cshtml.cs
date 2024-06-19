5ca15227bad7c6f1b23d9defb2f8ad7f4e487372
NebGod11921 <134355572+NebGod11921@users.noreply.github.com>
2024-06-19 21:18:46 +0700

    commit login

--- GoodExchangeApplication/MyRazorPage/Pages/Account/login.cshtml.cs
+++ GoodExchangeApplication/MyRazorPage/Pages/Account/login.cshtml.cs
@@ -1,12 +1,46 @@
+using DataAccessObjects.IServices;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+
 
 namespace MyRazorPage.Pages.Account
 {
     public class loginModel : PageModel
     {
-        public void OnGet()
+        private readonly IAccountService _accountService;
+
+        public loginModel(IAccountService accountService)
         {
+            _accountService = accountService;
+        }
+
+
+        public async Task<IActionResult> OnPostLogin(string Email, string Password)
+        {
+            ViewData["Email"] = Email;
+            ViewData["Password"] = Password;
+            try
+            {
+                if (string.IsNullOrEmpty(Password) && string.IsNullOrEmpty(Email))
+                {
+                    return Page();
+                }
+                else
+                {
+                    var result = _accountService.LoginAccountAsync(Email, Password);
+                    if (result != null)
+                    {
+                        return RedirectToPage("/Index");
+                    } else
+                    {
+                        return Page();
+                    }
+                }
+
+            }catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
     }
 }
