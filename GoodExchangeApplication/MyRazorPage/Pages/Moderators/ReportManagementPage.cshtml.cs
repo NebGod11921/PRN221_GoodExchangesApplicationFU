2a021fc00eff6d9374ca268aa417e26b3418f06a
NebGod11921 <134355572+NebGod11921@users.noreply.github.com>
2024-06-21 23:18:45 +0700

    commit layout for the website

---
+++ GoodExchangeApplication/MyRazorPage/Pages/Moderators/ReportManagementPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyRazorPage.Pages.Moderators
+{
+    public class ReportManagementPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
