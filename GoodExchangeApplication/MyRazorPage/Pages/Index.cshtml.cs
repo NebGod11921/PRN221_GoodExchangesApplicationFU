95dfd33340b1ae9f508ac72fb435f370e6631b58
Đỗ Nguyễn Đăng Khoa <khoadndse173397@fpt.edu.vn>
2024-06-26 16:45:02 +0700

    done homepage,productdetails

--- GoodExchangeApplication/MyRazorPage/Pages/Index.cshtml.cs
+++ GoodExchangeApplication/MyRazorPage/Pages/Index.cshtml.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessObjects;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace MyRazorPage.Pages
 {
@@ -14,7 +17,12 @@ namespace MyRazorPage.Pages
 
         public void OnGet()
         {
-
+                
+        }
+        public IActionResult OnPostLogout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToPage("/Index");
         }
     }
 }
---
+++ GoodExchangeApplication/MyRazorPage/Pages/Transaction/CheckOut.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyRazorPage.Pages.Order
+{
+    public class CheckOutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
