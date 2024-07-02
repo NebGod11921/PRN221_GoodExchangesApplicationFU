790b4bcdbcdf0c28660c028fc822f58dd5a7d1f1
KhoaVD <you@example.com>
2024-07-02 15:57:06 +0700

    CRUD Product Management

--- GoodExchangeApplication/BusinessObjects/obj/Debug/net7.0/BusinessObjects.AssemblyInfo.cs
+++ GoodExchangeApplication/BusinessObjects/obj/Debug/net7.0/BusinessObjects.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessObjects")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d01de52fc6f1fb90951afd7f3e5d652a0a09745")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessObjects")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessObjects")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
