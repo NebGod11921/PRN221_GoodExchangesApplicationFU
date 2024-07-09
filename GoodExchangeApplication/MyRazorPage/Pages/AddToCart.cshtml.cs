3e457e750be4fa2f2c178bcb63eef0b2533cc63e
NebGod11921 <134355572+NebGod11921@users.noreply.github.com>
2024-07-10 01:06:42 +0700

    REmove cart

--- GoodExchangeApplication/MyRazorPage/Pages/AddToCart.cshtml.cs
+++ GoodExchangeApplication/MyRazorPage/Pages/AddToCart.cshtml.cs
@@ -1,4 +1,6 @@
 
+using DataAccessObjects.Helpers;
+using DataAccessObjects.IServices;
 using DataAccessObjects.ViewModels.CartDTOS;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -8,14 +10,27 @@ namespace MyRazorPage.Pages.Transaction
 {
     public class AddToCartModel : PageModel
     {
-
-
+        
+        public IEnumerable<CartDTOs> CartDTOs { get; set; }
 
 
         
 
 
-        public IEnumerable<CartDTOs> CartDTOs { get; set; }
+
+        public List<CartDTOs> Carts
+        {
+            get
+            {
+                var data = HttpContext.Session.Get<List<CartDTOs>>("MyCart");
+                if (data == null)
+                {
+                    data = new List<CartDTOs>();
+                }
+                return data;
+            }
+        }
+
 
 
         public void OnGet()
@@ -25,11 +40,37 @@ namespace MyRazorPage.Pages.Transaction
             {
                 var json = JsonSerializer.Deserialize<IEnumerable<CartDTOs>>(getSession);
                 CartDTOs = json;
+            } else
+            {
+                CartDTOs = new List<CartDTOs>();
             }
 
         }
+        
 
 
+
+        public IActionResult OnPostRemoveCart(int itemId)
+        {
+            try
+            {
+                var cart = Carts;
+                var item = cart.SingleOrDefault(x => x.Id == itemId);
+                if (item != null) 
+                {
+                    cart.Remove(item);
+                    var Json = JsonSerializer.Serialize(cart);
+                    HttpContext.Session.SetString("MyCart", Json);
+                }
+                return RedirectToPage("/AddToCart");
+
+
+
+            }catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
 
 
 
--- GoodExchangeApplication/MyRazorPage/Pages/Homepage/ProductDetailPage.cshtml.cs
+++ GoodExchangeApplication/MyRazorPage/Pages/Homepage/ProductDetailPage.cshtml.cs
@@ -34,7 +34,7 @@ namespace MyRazorPage.Pages.Homepage
                 return data;
             }
         }
-        
+ 
 
 
 
