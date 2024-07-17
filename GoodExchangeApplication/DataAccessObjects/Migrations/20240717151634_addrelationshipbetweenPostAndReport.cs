08141f034b7d466aa10e104a3a28d8b39562d082
NebGod11921 <134355572+NebGod11921@users.noreply.github.com>
2024-07-17 22:19:11 +0700

    fix migration

---
+++ GoodExchangeApplication/DataAccessObjects/Migrations/20240717151634_addrelationshipbetweenPostAndReport.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessObjects.Migrations
+{
+    /// <inheritdoc />
+    public partial class addrelationshipbetweenPostAndReport : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PostId",
+                table: "Reports",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reports_PostId",
+                table: "Reports",
+                column: "PostId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reports_Posts_PostId",
+                table: "Reports",
+                column: "PostId",
+                principalTable: "Posts",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reports_Posts_PostId",
+                table: "Reports");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reports_PostId",
+                table: "Reports");
+
+            migrationBuilder.DropColumn(
+                name: "PostId",
+                table: "Reports");
+        }
+    }
+}
