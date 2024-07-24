475135f9c796b95a52d85f0458caa9247a7a151c
KhoaVD <you@example.com>
2024-07-24 22:07:48 +0700

    Fix small things

--- GoodExchangeApplication/DataAccessObjects/obj/Debug/net7.0/DataAccessObjects.AssemblyInfo.cs
+++ GoodExchangeApplication/DataAccessObjects/obj/Debug/net7.0/DataAccessObjects.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccessObjects")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15de2fe5cbedc7d19dadd9334fe92f09d71d2439")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccessObjects")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccessObjects")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- GoodExchangeApplication/MyRazorPage/obj/Debug/net7.0/MyRazorPage.AssemblyInfo.cs
+++ GoodExchangeApplication/MyRazorPage/obj/Debug/net7.0/MyRazorPage.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyRazorPage")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15de2fe5cbedc7d19dadd9334fe92f09d71d2439")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyRazorPage")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyRazorPage")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
